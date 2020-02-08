a15dba64b99f67c975c8ccc09fd21a0d3afd8d2c
TaChyla <tachylad.murray@gmail.com>
2020-02-07 17:46:20 -0800

    adding more dependencies

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const app = express();
 app.post('/sms', (req, res) => {
     const twiml = new MessagingResponse();
 
-    twiml.message('The Robots are coming! Head for the hills!');
+    twiml.message('What you thought? My app wouldn\'t work?');
 
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
 });
 
-http.createServer(app).listen(1337, () => {
-    console.log('Express server listening on port 1337');
+
+
+http.createServer(app).listen(8080, () => {
+    console.log('Express server listening on port 8080');
 });
