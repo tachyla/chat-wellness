92e18c308d2f0dc99978bacdf34e3e727b754eb1
TaChyla <tachylad.murray@gmail.com>
2020-02-10 16:40:32 -0800

    update dependencies

--- send_sms.js
+++ send_sms.js
@@ -1,5 +1,3 @@
-// Download the helper library from https://www.twilio.com/docs/node/install
-// DANGER! This is insecure. See http://twil.io/secure
 require('dotenv').config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTHENTICATION_TOKEN;
@@ -8,9 +6,9 @@ const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
-     body: 'If you got this text, you want to subscribe to something TaChyla has to say?',
+     body: 'If you got this text, you want to subscribe to something TaChyla has to say...',
      from: '+16782632882',
+     // phone number can be replaced with the contacting number or userid
      to: '+16785424119'
    })
   .then(message => console.log(message.sid));
-
