22e186e90c487b182bf328e8cf9d4d8a20ebf883
TaChyla <tachylad.murray@gmail.com>
2020-02-14 17:12:48 -0800

    added src for components

---
+++ client/src/components/card.js
@@ -0,0 +1,12 @@
+//stateful component to maintain friends being added to contacts
+export default class Card extends React.Component {
+    render() {
+    const text  = 'Top card';
+    return (
+        <div className="card">
+        {hello from card}
+        </div>
+    );
+    }
+}
+
---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './components/card';
+import './index.css';
+
+ReactDOM.render(
+    <Card/>,
+    document.getElementById('root')
+);
